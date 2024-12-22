f050fc4c1f2b33a6f5ce0345607cfdc0c7340854
jörn <joern@keepout.de>
2024-12-23 00:46:53 +0100

    Add interesting weblinks

--- keyboards/handwired/dactyl_promicro/applyQmkConfiguratorFile.sh
+++ keyboards/handwired/dactyl_promicro/applyQmkConfiguratorFile.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# https://config.qmk.fm/#/test
+# https://config.qmk.fm/#/handwired/dactyl_promicro/LAYOUT_6x6
+
 cd ./keymaps/default
 mv ~/Downloads/handwired_dactyl_promicro_layout_6x6_mine.json ./layout-source.json
 qmk json2c layout-source.json > keymap.c
